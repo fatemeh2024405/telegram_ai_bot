edaf453b37487a367a7791109c5e21803a36869b
fatemeh2024405 <fatemeh2024405@gmail.com>
2026-06-26 04:04:23 +0330

    add bot.py file

---
+++ bot.py
@@ -0,0 +1,33 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, MessageHandler, ContextTypes, filters
+from openai import OpenAI
+
+client = OpenAI(
+    api_key="bot_token",
+    base_url="https://api.groq.com/openai/v1"
+)
+
+async def chat(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_message = update.message.text
+
+    response = client.chat.completions.create(
+        model="llama3-8b-8192",
+        messages=[
+            {"role": "user", "content": user_message}
+        ]
+    )
+
+    await update.message.reply_text(
+        response.choices[0].message.content
+    )
+
+app = ApplicationBuilder().token(
+    "groq_api_key"
+).build()
+
+app.add_handler(
+    MessageHandler(filters.TEXT & ~filters.COMMAND, chat)
+)
+
+print("Bot started!")
+app.run_polling()
